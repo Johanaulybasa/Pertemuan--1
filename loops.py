05b063e27e44e2cf3ef350c2243e931604e7884a
Johanaulybasa <132275006+Johanaulybasa@users.noreply.github.com>
2023-06-30 19:44:57 +0700

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,16 @@
+x =7
+while x > 0:
+    print("positive x=" + str(x))
+    x = x - 1
+print("now x =" + str(x))
+
+for x in range(3):
+    print("x=" + str(x))
+x =7
+while x > 0:
+    print("positive x=" + str(x))
+    x = x - 1
+print("now x =" + str(x))
+
+for x in range(3):
+    print("x=" + str(x))
